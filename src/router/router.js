75345a3983122ca4f1be9b6949d542f7de92b3ca
Juan <juangrondona.01@gmail.com>
2023-01-31 12:55:41 -0300

    Agregue token a cliente

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,10 @@
 const express= require('express');
 const router = express();
 const cors = require('cors');
+// para encriptar password
+const bcrypt= require('bcrypt');
+// Para generar token
+const jwt= require('jsonwebtoken');
 
 //////archivo de coneccion
 const mysqlConeccion = require('../database/database');
@@ -11,39 +15,57 @@ router.get('/', (req, res)=>{
     res.send('Pantalla Inicio de App');
 });
 //Devuelve a todos los clientes activos de nuestra base de datos 
-router.get('/cliente',(req, res)=>{
-        const query='select * from cliente';
-            mysqlConeccion.query(query, (err, rows)=>{
+router.get('/cliente', verificarToken, (req, res)=>{
+    jwt.verify(req.token, 'bazarKey', (error, valido)=>{
+        if(error){
+            res.sendStatus(403);
+        }else{
+        mysqlConeccion.query('select * from cliente', (err, registro)=>{
+            if(!err){
+
+                res.json(registro);
+            }else{
+                console.log(err)
+            }
+        })
+        }
+    })
+});
+//Devuelve a un cliente puntual
+router.get('/cliente/:id', verificarToken, (req, res)=>{
+    const  { id } = req.params;
+    jwt.verify(req.token, 'bazarKey', (error, valido)=>{
+        if(error){
+            res.sendStatus(403);
+        }else{
+            mysqlConeccion.query('select * from cliente where id=?',[id], (err, registros)=>{
                 if(!err){
-                    res.json(rows);
+                    res.json(registros);
                 }else{
                     console.log(err)
                 }
             })
-        });    
-//Devuelve a un cliente puntual
-router.get('/cliente/:id',(req, res)=>{
-    const {id} = req.params;
-      mysqlConeccion.query('select * from cliente where id = ?',[id], (err, rows)=>{
-        if(!err){
-            res.json(rows);
-        }else{
-            console.log(err)
         }
     })
 });
 //metodo para insertar cliente por el metodo POST
-router.post('/cliente',(req, res)=>{
+router.post('/cliente', verificarToken, (req, res)=>{
     const { nombre, apellido, estado } =req.body
-    let query=`INSERT INTO cliente (nombre, apellido, estado, tms) VALUES ('${nombre}','${apellido}','${estado}',NOW())`;
-            mysqlConeccion.query(query, (err)=>{
+    jwt.verify(req.token, 'bazarKey', (error, valido)=>{
+        if(error){
+            res.sendStatus(403);
+        }else{
+            let query=`INSERT INTO cliente (nombre, apellido, estado, tms) VALUES ('${nombre}','${apellido}','${estado}',NOW())`;
+            mysqlConeccion.query(query, (err, registros)=>{
                 if(!err){
-                    res.send('Se inserto correctamente nuestros datos');
+                    res.send('Se inserto correctamente nuestro dato: '+nombre);
                 }else{
                     console.log(err)
                 }
             })
-        });
+        }
+    })
+});
 //Se agrega metodo para actualizar datos del cliente por el metodo PUT
 router.put('/cliente/:id',(req, res)=>{
     let id = req.params.id
@@ -81,16 +103,45 @@ router.get('/productos',(req, res)=>{
         })
     });    
 //Devuelve un producto puntual
-router.get('/productos/:id',(req, res)=>{
-    const {id} = req.params;
-      mysqlConeccion.query('select * from productos where id = ?',[id], (err, rows)=>{
-        if(!err){
-            res.json(rows);
-        }else{
-            console.log(err)
-        }
-    })
-});
+router.get('/productos/:id', verificarToken,(req, res)=>{
+    // const {id} = req.params;
+    const  parametro  = req.params.id
+    if(esNumero(parametro)){
+        res.json(
+            {
+                status: false,
+                mensaje:"El parametro que se espera tiene ser un numero entero"
+            });
+    }else{
+        jwt.verify(req.token, 'bazarKey', (error, valido)=>{
+            if(error){
+                // console.log(' entra aca')
+                res.sendStatus(403);
+            }else{
+                mysqlConeccion.query('select * from productos where id=?',[parametro], (err, rows)=>{
+                    if(!err){
+                        if(rows.length!=0){
+                            res.json(rows);
+                        }else{
+                            res.json(
+                                {
+                                    status: false,
+                                    mensaje:"El ID del producto no existe en la base de datos."
+                                });
+                        }    
+                    }else{
+                        res.json(
+                        {
+                            status: false,
+                            mensaje:"Error en el servidor."
+                        });
+                    }
+                });
+                
+            }
+        });
+    }
+})
 //metodo para insertar productos por el metodo POST
 router.post('/productos',(req, res)=>{
     const { nombre, descripcion, id_marca, precio_costo, precio_venta, estado, stock } =req.body
@@ -128,39 +179,130 @@ router.delete('/productos/:id',(req, res)=>{
         }
     })
 });
-////////////// USUARIOS //////////////
-//Devuelve a todos los usuarios activos de nuestra DB 
-router.get('/usuarios',(req, res)=>{
-        const query='select * from usuarios';
-            mysqlConeccion.query(query, (err, rows)=>{
-                if(!err){
-                    res.json(rows);
-                }else{
-                    console.log(err)
-                }
-            })
-        });    
-//Devuelve a un usuario puntual
-router.get('/usuarios/:id_usuario',(req, res)=>{
-    const {id} = req.params;
-      mysqlConeccion.query('select * from usuarios where id_usuario = ?',[id], (err, rows)=>{
-        if(!err){
-            res.json(rows);
-        }else{
-            console.log(err)
-        }
-    })
+////////////// /////////////////
+//////////////Usuarios /////////
+////////////// /////////////////
+router.get('/usuarios', verificarToken, (req, res)=>{
+
+    jwt.verify(req.token, 'bazarKey', (error, valido)=>{
+    if(error){
+        res.sendStatus(403);
+    }else{
+        mysqlConeccion.query('select * from usuarios', (err, registro)=>{
+    if(!err){
+        // console.log(registro.length)
+        res.json(registro);
+    }else{
+        console.log(err)
+    }
+})
+}
+
+})
+
 });
-//metodo para crear usuario por el metodo POST
-router.post('/registro',(req, res)=>{
-    const {username, password, email, apellido_nombre} =req.body
-    let query=`INSERT INTO usuarios (username, password, email, apellido_nombre, fecha_creacion) VALUES ('${username}','${password}','${email}','${apellido_nombre}',NOW())`;
-            mysqlConeccion.query(query, (err)=>{
-                if(!err){
-                    res.send('Se inserto correctamente nuevo usuario '+apellido_nombre);
+
+////////////login de usuarios //////////////
+router.post('/login', (req, res)=>{
+const {username, password} =req.body
+if(username!=undefined && password!=undefined){
+    mysqlConeccion.query('select u.id_usuario, u.username,  u.password,  u.email, u.apellido_nombre from usuarios u where u.estado="1" AND username=?',[username], (err, rows)=>{
+        if(!err){
+            if(rows.length!=0){
+                const bcryptPassword = bcrypt.compareSync(password, rows[0].password);
+                if(bcryptPassword){
+                    jwt.sign({rows}, 'bazarKey', {expiresIn:'1200s'},(err, token)=>{
+                        res.json(
+                            {
+                                status: true,
+                                datos: rows,
+                                token: token
+                            });
+                    }) 
                 }else{
-                    res.send('Ocurrio un error en el servidor '+err);
+                    res.json(
+                        {
+                            status: false,
+                            mensaje:"La Contraseña es incorrecta"
+                        });
                 }
-            })
-        });
+            }else{
+                res.json(
+                    {
+                        status: false,
+                        mensaje:"El usuario no existe "
+                    });
+                
+            }
+        }else{
+            res.json(
+                {
+                    status: false,
+                    mensaje:"Error en el servidor"
+                });
+            
+        }
+    });
+}else{
+    res.json({
+        status: false,
+        mensaje:"Faltan completar datos"
+    });
+}
+});
+////////////login de usuarios //////////////
+router.post('/registro', async(req, res)=>{
+const {username, password, email, apellido_nombre} =req.body
+let hash = bcrypt.hashSync(password,10);
+
+let query=`INSERT INTO usuarios (username, password, email, apellido_nombre, fecha_creacion) VALUES ('${username}','${hash}','${email}','${apellido_nombre}',NOW())`;
+mysqlConeccion.query(query, (err, registros)=>{
+    if(!err){
+        res.send('Se inserto correctamente nuestro usuario: '+username);
+    }else{
+        res.send('Ocurrio un error desde el servidor'+err);
+    }
+})
+});
+
+router.put('/resetpassword/:id', (req, res)=>{
+// asigna a id_usuario el valor que recibe por el parametro 
+ let id  = req.params.id;
+// //asigna el valor que recibe  en el Body 
+ const { password } =req.body 
+ let hash = bcrypt.hashSync(password,10); 
+//  generamos la query de modificacion del password
+ let query=`UPDATE usuarios SET password='${hash}' WHERE id='${id}'`;
+ mysqlConeccion.query(query, (err, registros)=>{
+    if(!err){
+        res.send('El Id que editamos es : '+id+' y cambiamos el password! Muchas gracias!');
+    }else{
+        console.log(err)
+    }
+})
+
+
+});
+////////////// /////////////////
+// //////////////////////Nuestras funciones /////////
+function verificarToken(req, res, next){
+const BearerHeader= req.headers['authorization']
+if(typeof BearerHeader!=='undefined'){
+    const bearerToken= BearerHeader.split(" ")[1]
+    req.token=bearerToken;
+    next();
+}else{
+     res.send('Para consultar las apis debe estar autenticado.Gracias');
+    // console.log('Ocurrio un error')
+}
+}
+
+function esNumero(parametro) {
+if(!isNaN(parseInt(parametro))){
+    return false
+} else {
+    return true
+}
+}
+
 module.exports = router;
